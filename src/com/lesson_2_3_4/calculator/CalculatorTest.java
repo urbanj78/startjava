ced5f1a10897076b161405f643573063e1ed2af8
Eugene <urbanj@yandex.ru>
2021-05-25 12:10:28 +0500

    hw upd 3 25.05.21

--- src/com/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/lesson_2_3_4/calculator/CalculatorTest.java
@@ -8,23 +8,24 @@ public class CalculatorTest {
         Calculator calculatorOne = new Calculator();
         String answer = "yes";
 
-        while(answer.equals("yes")) {
-            System.out.println("Введите математическое выражение");
-            String mathExp = scanner.nextLine();
-            String[] mathExpSymbols = mathExp.split(" ");
-            int num1 = Integer.parseInt(mathExpSymbols[1]);
-            char mathSign = mathExpSymbols[2].charAt(0);
-            int num2 = Integer.parseInt(mathExpSymbols[3]);
+         {
+             do {
+                 System.out.println("Введите математическое выражение");
+                 String mathExp = scanner.nextLine();
+                 String[] mathExpSymbols = mathExp.split(" ");
+                 int num1 = Integer.parseInt(mathExpSymbols[0]);
+                 char mathSign = mathExpSymbols[1].charAt(0);
+                 int num2 = Integer.parseInt(mathExpSymbols[2]);
 
-            int result = calculatorOne.calculate(num1, num2, mathSign);
+                 int result = calculatorOne.calculate(num1, num2, mathSign);
 
-            System.out.println(num1 + " " + mathSign + " " + num2 + " = " + result);
-            answer = "";
+                 System.out.println(num1 + " " + mathSign + " " + num2 + " = " + result);
 
-            do {
-                System.out.println("Хотите продолжить вычисления? [yes/no]:");
-                answer = scanner.nextLine();
-            } while(!answer.equals("yes") && !answer.equals("no"));
+                 do {
+                     System.out.println("Хотите продолжить вычисления? [yes/no]:");
+                     answer = scanner.nextLine();
+                 } while (!answer.equals("yes") && !answer.equals("no"));
+             } while (answer.equals("yes"));
         }
     }
 }
