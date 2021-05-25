f10ae43137e076107be7269141f23bae2920f77e
Eugene <urbanj@yandex.ru>
2021-05-25 12:01:05 +0500

    hw upd 2 25.05.21

--- src/com/lesson_2_3_4/calculator/Calculator.java
+++ src/com/lesson_2_3_4/calculator/Calculator.java
@@ -1,27 +1,20 @@
 package com.lesson_2_3_4.calculator;
 
 public class Calculator {
-    static int num1 = 0;
-    static int num2 = 0;
-    static char mathSign = '+';
-
     public int calculate(int num1, int num2, char mathSign) {
-        this.num1 = num1;
-        this.num2 = num2;
-        this.mathSign = mathSign;
         switch(mathSign) {
-                case '+':
-                    return num1 + num2;
-                case '-':
-                    return num1 - num2;
-                case '*':
-                    return num1 * num2;
-                case '/':
-                    return num1 / num2;
-                case '^':
-                    return (int)Math.pow(num1, num2);
-                case '%':
-                    return (int)Math.IEEEremainder(num1, num2);
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                return num1 / num2;
+            case '^':
+                return (int) Math.pow(num1, num2);
+            case '%':
+                return (int) Math.IEEEremainder(num1, num2);
         }
         return 0;
     }
